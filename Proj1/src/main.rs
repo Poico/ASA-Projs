12b07503a38aa2ef7722b65e94ac6dddc4016e78
zeserrado-marques <57963638+zeserrado-marques@users.noreply.github.com>
2023-11-15 13:41:33 +0000

    Update main.rs
    add comment

--- Proj1/src/main.rs
+++ Proj1/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let mut iter = input.split_whitespace();
-    let _x: i32 = iter.next().unwrap().parse().unwrap();
+    let _x: i32 = iter.next().unwrap().parse().unwrap(); // protects from code injection
     let _y: i32 = iter.next().unwrap().parse().unwrap();
     
 }
