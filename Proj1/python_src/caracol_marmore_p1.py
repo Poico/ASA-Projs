ed7ab87dd83cbfb62bfe2130ba82d317707b9657
poicoLaptop <rodrigodecgomes@gmail.com>
2023-11-23 15:31:00 +0000

    Probably finished

--- Proj1/python_src/caracol_marmore_p1.py
+++ Proj1/python_src/caracol_marmore_p1.py
@@ -1,6 +1,6 @@
 # código teste para o proj1 de asa
 from queue import PriorityQueue
-import math
+
 import copy
 
 def mostra_matriz(matriz : list) -> str: 
@@ -11,10 +11,12 @@ def mostra_matriz(matriz : list) -> str:
     return out.rstrip() + "]"
 
 
-def valor_mais_macho(matriz : list, slabs_values : list) -> int: 
+def valor_mais_macho(matriz : list) -> int: 
     
     #TODO: add variable for the len <- reduce time calculating
-    
+    # 3x1 = 2
+    # 1x1 = 3
+    # soma do coiso mais o max
     for line in range(1,len(matriz)):
         for column in range(1,len(matriz[line])):
             # max de 1 até n peças
@@ -22,21 +24,21 @@ def valor_mais_macho(matriz : list, slabs_values : list) -> int:
             lst = []
             # get value calculated before for each possible cut (either in X or Y)
             for k in range(1,line+1):
-                print(f"line:{line} column:{column} k:{k}", end="\n")
-                print(matriz[line-k][column], slabs_values[line][column], end="\n")
-                lst.append(matriz[line-k][column] + slabs_values[line][column])
+                #print(f"line:{line} column:{column} k:{k}", end="\n")
+                #print(matriz[line-k][column], slabs_values[line][column], end="\n")
+                lst.append(matriz[line-k][column] + matriz[line-(line-k)][column])
+                #lst.append(matriz[line-k][column] + matriz[k-line][column])
+                #lst.append(matriz[line-k][column] + slabs_values[line][column])
+                
             for k in range(1, column+1):
-                print(f"line:{line} column:{column} k:{k}", end="\n")
-                print(matriz[line][column-k], slabs_values[line][column], end="\n")
-                lst.append(matriz[line][column-k] + slabs_values[line][column])
-            
-            
+                #print(f"line:{line} column:{column} k:{k}", end="\n")
+                #print(matriz[line][column-k], slabs_values[line][column], end="\n")
+                lst.append(matriz[line][column-k] + matriz[line][column-(column-k)])
+                #lst.append(matriz[line][column-k] + slabs_values[line][column])
+            print(lst)
             matriz[line][column] = max(lst)
 
     return matriz[len(matriz)-1][len(matriz[0])-1]
-
-
-    
 
 def main():
     """
@@ -54,15 +56,15 @@ def main():
         for _ in range(columns+1):
             matriz[i].append(0)
     
-    slabs_values = copy.deepcopy(matriz)
+    
     
     slabs_n = int(input())
 
     for _ in range(slabs_n):
         x, y, value = map(int, input().split()) # x e y são tamanhos e value é valor da peça x
-        slabs_values[x][y] = value
+        matriz[x][y] = value
         try:
-            slabs_values[y][x] = value
+            matriz[y][x] = value
         except IndexError:
             pass
 
@@ -70,9 +72,13 @@ def main():
     # algoritmo para calcular valor máximo
     
     #print(mostra_matriz(slabs_values))
-    print(valor_mais_macho(matriz, slabs_values))
+    
+    print(valor_mais_macho(matriz))
+    #1x1 3
+    
+    #2x1 2
 
-    print(mostra_matriz(matriz), end="\n\n")
+    #print(mostra_matriz(matriz), end="\n\n")
 
 
 if __name__ == "__main__":
