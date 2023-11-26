45dc01b83175234b77ed3fa0439c882f298610c3
Poico <rodrigodecgomes@gmail.com>
2023-11-26 22:15:17 +0000

    Code in cpp

--- Proj1/cpp_src/proj1.cpp
+++ Proj1/cpp_src/proj1.cpp
@@ -1,11 +1,42 @@
 #include <stdio.h>
+
 using namespace std;
 
 int main(){
-  int n, m;
-  scanf("%d,%d", &n, &m);
-  int matrix[n][m] = {0};
-  int cuts_amount;
-  scanf("%d",&cuts_amount);
-  return 0;
+    int lin,col;
+    int cutNumber;
+    int line, column,price;
+    scanf("%d %d", &lin , &col);
+    lin+=1;
+    col+=1;
+    int** matrix = new int*[lin];
+    for (int i = 0; i < lin; ++i) {
+        matrix[i] = new int[col];
+    }
+    scanf("%d", &cutNumber);
+    for(int i = 0; i < cutNumber; i++) {
+        scanf("%d %d %d", &line, &column, &price);
+        if(line < lin && column < col ) {
+            matrix[line][column]=price;
+        } 
+
+        if (column < lin && line < col) {
+            matrix[column][line]=price;
+        }
+    } 
+    int maximo;
+    for(int line=1; line < lin; line++){
+        for(int column=1;column<col;column++){
+            maximo=0;
+            for(int k=1;k<=line;k++)
+                if (maximo<(matrix[line-k][column] + matrix[line-(line-k)][column]))
+                    maximo = (matrix[line-k][column] + matrix[line-(line-k)][column]);
+            for(int k=1;k<=column;k++)
+                if (maximo<(matrix[line][column-k] + matrix[line][column-(column-k)]))
+                    maximo = matrix[line][column-k] + matrix[line][column-(column-k)];
+            matrix[line][column]=maximo;
+        }
+    }
+    printf("%d\n",matrix[lin-1][col-1]);
+    return 0;
 }
