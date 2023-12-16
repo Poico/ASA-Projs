fc68892abaa6e883f7284e9aa800cb4edd4236d4
zeserrado-marques <zeserradommarques@gmail.com>
2023-12-16 16:06:26 +0000

    nome representativo do propósito do projecto

--- p2/cpp_src/grafos_covid.cpp
+++ p2/cpp_src/grafos_covid.cpp
@@ -1,6 +1,6 @@
 /*
 Objectivos:
-- Tarjan (Ou 2 DFS) para identificar os SCCs 
+- 2 DFS para identificar os SCCs 
 - Construir o Grafo dos SCCs
 - DFS no grafo SCCs para achar ordem topológica
 - Árvore com maior profundidade na floresta DFS
