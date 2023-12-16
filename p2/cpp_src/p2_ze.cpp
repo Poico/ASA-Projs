9688feb23e422fe90974465f7c5cf2ee03115ae5
zeserrado-marques <zeserradommarques@gmail.com>
2023-12-16 15:16:45 +0000

    criação de tests e começar a sofrer

---
+++ p2/cpp_src/p2_ze.cpp
@@ -0,0 +1,15 @@
+/*
+Objectivos:
+- Tarjan (Ou 2 DFS) para identificar os SCCs 
+- Construir o Grafo dos SCCs
+- DFS no grafo SCCs para achar ordem topológica
+- Árvore com maior profundidade na floresta DFS
+
+flags de compilação:  g++ -std=c++11 -O3 -Wall file.cpp -lm
+*/
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
---
+++ p2/python_src/virus_caracol.py
@@ -0,0 +1,17 @@
+
+'''
+Objectivos:
+- Tarjan para identificar os SCCs
+- Construir o Grafo dos SCCs
+- DFS no grafo SCCs para achar ordem topológica
+- Árvore com maior profundidade na floresta DFS
+'''
+
+
+
+def main():
+    print("assim, sim, com uma main é que é")
+
+
+if __name__ == "__main__":
+    main()
