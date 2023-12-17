933e00aaceac70e00571fbd1c97a31c69f3a87f7
poicoLaptop <rodrigodecgomes@gmail.com>
2023-12-17 18:44:43 +0000

    SCC

--- p2/cpp_src/grafos_covid.cpp
+++ p2/cpp_src/grafos_covid.cpp
@@ -22,9 +22,12 @@ enum Colors { WHITE, GRAY, BLACK };
 void DFSvisitInputGraph(int vertice, int *time,
                         stack<pair<int, int>> *st_secondDFS, enum Colors *color,
                         int *end_time, int *parent);
+void DFSvisitTransposedGraph(int vertice, int SCC_num, int *SCCs,
+                             enum Colors *color, int *time, int *end_time);
+
 void addEdge();
 int dfsIterative(int start);
-void printGraph(vector<vector<int>> graph);
+void printGraph(vector<vector<int>> graph, int vertices);
 void printGraphMatrix(vector<vector<int>> graph);
 void resetColors(enum Colors *color);
 
@@ -71,7 +74,7 @@ int main(int argc, char const *argv[]) {
   int time = 0;
   for (int u = 1; u <= vertices; u++) {
     if (color[u] == WHITE) {
-      printf("DFS no vertice %d\n", u);
+      // printf("DFS no vertice %d\n", u);
       DFSvisitInputGraph(u, &time, &st_secondDFS, color, end_time, parent);
       // to insert the nodes for the next DFS correctly
     }
@@ -79,21 +82,41 @@ int main(int argc, char const *argv[]) {
 
   // second DFS to identify SCCs
   int SCC_num = 1;
+  time = 0;
   resetColors(color);
+
   while (!st_secondDFS.empty()) {
     pair<int, int> p = st_secondDFS.top();
     int u = p.first;
-    if (color[u] == WHITE)
-      DFSvisitTransposedGraph(u, SCCs, color);
+    st_secondDFS.pop();
+    if (color[u] == WHITE) {
+      DFSvisitTransposedGraph(u, SCC_num, SCCs, color, &time, end_time);
+      ++SCC_num;
+    }
   }
-  
 
+  // init SCCs graph
+  vector<vector<int>> graph_sccs;
+  graph_sccs.resize(SCC_num+1);
+  for (int i = 1; i <= SCC_num; i++) {
+    graph_sccs[i].resize(SCC_num);
+  }
+  // create edges for the graph
+  for (int i = 1; i <=vertices; i++) {
+    for (int j = 1; j <= vertices; j++){
+      if (graph_input[i][j] != 0 && graph_sccs[SCCs[i]][SCCs[j]] == 0 && SCCs[i] != SCCs[j])
+        graph_sccs[SCCs[i]][SCCs[j]] = 1;
+    }
+  }
 
-  // for (int i = 1; i <= vertices; i++) {
-  //   printf("for vertice %d: parent - %d, tempo de fim - %d\n", i, parent[i], end_time[i]);
-  // }
-  
-  
+  printGraph(graph_sccs, SCC_num);
+
+  // final DFS
+  resetColors(color);
+  for (int u = 1; u <= vertices; u++) {
+    if (color[u] == WHITE) {
+    }
+  }
   // printf("Longest path: %d\n", dfsIterative(1));
 
   return 0;
@@ -135,16 +158,51 @@ void DFSvisitInputGraph(int vertice, int *time,
       ++(*time);
       color[u] = BLACK;
       end_time[u] = *time;
-      printf("fechei o vertice %d com tempo de fim %d e predecessor %d\n", u,
-             end_time[u], parent[u]);
+      // printf("fechei o vertice %d com tempo de fim %d e predecessor %d\n", u,
+            //  end_time[u], parent[u]);
       st_secondDFS->push(make_pair(u, 0));
       st.pop(); // finished verifying current node, so we take it out
     }
   }
-  printf("acabei de fazer a árvore de %d\n", vertice);
+  // printf("acabei de fazer a árvore de %d\n", vertice);
 }
 
-void DFSvisitTransposedGraph(int vertice, )
+void DFSvisitTransposedGraph(int vertice, int SCC_num, int *SCCs,
+                             enum Colors *color, int *time, int *end_time) {
+  // pair of vertice u and vertice where we left off on the DFSvisit of u
+  ++(*time);
+  stack<pair<int, int>> st;
+  pair<int, int> initial(vertice, 0);
+  st.push(initial);
+  color[vertice] = GRAY;
+
+  while (!st.empty()) {
+    pair<int, int> p = st.top();
+    int u = p.first;
+
+    // visit adjacents, if there's one yet unexplored, we go through it next
+    while (st.top().second < vertices) {
+      ++st.top().second;
+      if (graph_transposed[u][st.top().second] != 0 &&
+          color[st.top().second] == WHITE) {
+        color[st.top().second] = GRAY;
+        ++(*time);
+        st.push(make_pair(st.top().second, 0));
+
+        break; // after we add it to the stack, we break from here, to look at
+               // the added node
+      }
+    }
+    // after i visited everyone, we close this vertice
+    if (st.top().second == vertices) {
+      ++(*time);
+      color[u] = BLACK;
+      end_time[u] = *time;
+      SCCs[u] = SCC_num;
+      st.pop(); // finished verifying current node, so we take it out
+    }
+  }
+}
 
 void addEdge() {
   // an edge that goes form u to v (u->v)
@@ -185,7 +243,7 @@ int dfsIterative(int start) {
 // debug coisos
 
 // pretty print
-void printGraph(vector<vector<int>> graph) {
+void printGraph(vector<vector<int>> graph, int vertices) {
   for (int u = 1; u <= vertices; u++) {
     for (int v = 1; v <= vertices; v++) {
       if (graph[u][v] != 0)
@@ -209,7 +267,7 @@ void printGraphMatrix(vector<vector<int>> graph) {
   printf("]\n");
 }
 
-void resetColors(enum Colors *color){
+void resetColors(enum Colors *color) {
   for (int i = 1; i <= vertices; i++) {
     color[i] = WHITE;
   }
